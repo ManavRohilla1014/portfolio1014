9e4f8bb97b9f4b99efd46a106a5896fd7a217a0c
Manav Rohilla <120121152+ManavRohilla1014@users.noreply.github.com>
2024-08-21 15:27:35 +0530

    Update TypingAnimation.tsx

--- src/components/TypingAnimation.tsx
+++ src/components/TypingAnimation.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
 const TypingAnimation = () => {
-    return <TypeAnimation sequence={["Full Stack Developer", 1000, "Devops Enthusiast", 1000]} wrapper="span" speed={50} className="font-bold lg:text-3xl text-blue-500" repeat={Infinity} />;
+    return <TypeAnimation sequence={["Full Stack Developer", 1000, "ML Enthusiast", 1000]} wrapper="span" speed={50} className="font-bold lg:text-3xl text-blue-500" repeat={Infinity} />;
 };
 
 export default TypingAnimation;
