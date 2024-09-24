1ccef91cf3dcc2c3df45572dd05a2d700ef6bca9
Manav Rohilla <120121152+ManavRohilla1014@users.noreply.github.com>
2024-09-24 17:03:56 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 export function generateMetadata() {
     return {
-        title: "About - Manav",
-        description: "Hi. I'm Manav Rohilla, a self-taught full stack developer from India. I'm a student currently pursuing my bachelors in Computer Science and Engineering, I love build software that solves real world problems which is probably why everything I build is Open Source.My everyday tech stack includes ReactJS , Tailwind , and so on ....",
+        title: "About - Shiva",
+        description: "Hi. I'm Shiva Bhattacharjee, a self-taught full stack developer from Guwahati Assam India. I'm a student currently pursuing my bachelors in Computer Science and Technology, I love building software that solves real world problems which is probably why everything I build is Open Source.My everyday tech stack includes NEXTJS , Typescript , Tailwind , and so on ....",
         openGraph: {
             images: "https://cdn.discordapp.com/attachments/1079039236302446705/1207210027333718096/SHIVA_1.png?ex=65ded0f3&is=65cc5bf3&hm=c511a0d118dae42adfc43114877d0689863f328da2dcc78c02826d271a5cd27f&",
         },
@@ -12,7 +12,6 @@ import { GitHubLogoIcon, InstagramLogoIcon, TwitterLogoIcon } from "@radix-ui/re
 import { Info, MailPlus, Zap } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaLinkedin } from "react-icons/fa";
 
 import aboutmemoji from "@/assets/about/confused-memoji.svg";
 const AboutPage = () => {
@@ -21,8 +20,8 @@ const AboutPage = () => {
             <div className="flex flex-col gap-4">
                 <h1 className=" text-3xl lg:text-5xl font-bold tracking-wide mt-7">
                     Hi. I&apos;m{" "}
-                    <a rel="noopener noreferrer" target="_blank" className=" text-balance text-blue-500">
-                        Manav{" "}
+                    <a href="https://twitter.com/sh17va" rel="noopener noreferrer" target="_blank" className=" text-balance text-blue-500">
+                        @sh17va{" "}
                     </a>
                     🤟
                 </h1>
@@ -33,25 +32,32 @@ const AboutPage = () => {
                 </h2>
                 <p className="text-md lg:text-2xl flex flex-col gap-2 whitespace-break-spaces font-normal">
                     <span>
-                        Hey , stranger 👋 I&apos;m Manav Rohilla, a self-taught full stack developer from India. I&apos;m a student currently pursuing my bachelors in <span className=" font-bold">Computer Science and Engineering </span>, I love build software that solves real world problems which is probably why everything I build is{" "}
-                        <a href="https://github.com/ManavRohilla1014" target="_blank" rel="noopener noreferrer" className=" font-bold text-green-500">
+                        Hey , stranger 👋 I&apos;m Shiva Bhattacharjee, a self-taught full stack developer from Guwahati Assam India. I&apos;m a student currently pursuing my bachelors in <span className=" font-bold">Computer Science and Technology</span>, I love building software that solves real world problems which is probably why everything I build is{" "}
+                        <a href="https://github.com/shivabhattacharjee" target="_blank" rel="noopener noreferrer" className=" font-bold text-green-500">
                             Open Source
                         </a>
                         .
                     </span>
 
                     <span>
-                        My everyday tech stack includes <span className="font-bold">ReactJS, Tailwind , and so on.... </span>(the list goes long lol 🔋)
+                        My everyday tech stack includes <span className="font-bold">NEXTJS , Typescript , Tailwind , and so on.... </span>(the list goes long lol 🔋)
                     </span>
                     <span>
                         My Operating System of choice is{" "}
+                        <a href="https://fedoraproject.org/" target="_blank" rel="noopener noreferrer" className=" text-lg text-blue-500 font-bold uppercase">
+                            Fedora
+                        </a>{" "}
+                        and{" "}
                         <a href="https://code.visualstudio.com/" target="_blank" rel="noopener noreferrer" className=" text-lg text-blue-500 font-bold uppercase">
                             VSCODE
                         </a>{" "}
                         for IDE .
                     </span>
                     <span>
-                        I am an active participant in contests and hackathons too as I love building protoypes in a rushing enviroment. 
+                        I am an active participant in hackathons too ( have won a few of them) as I love building protoypes in a rushing enviroment. I was also a finalist in{" "}
+                        <a href="https://www.sih.gov.in/#" className=" text-orange-400 font-bold" target="_blank" rel="noopener noreferrer">
+                            Smart India Hackathon 2023
+                        </a>{" "}
                     </span>
                     <span>
                         So if you are a hackathon organiser , looking for a teammate or just want to have a chat with me feel free to connect with me my socials are listed below , do drop a visit to my{" "}
@@ -73,38 +79,30 @@ const AboutPage = () => {
                     Lets Connect
                 </h2>
                 <div className="flex gap-2 flex-wrap mt-2 mb-2 items-center">
-                    <a href="https://github.com/ManavRohilla1014" target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/shivabhattacharjee" target="_blank" rel="noopener noreferrer">
                         <GitHubLogoIcon className=" h-6 lg:h-10 w-8 lg:w-8 duration-200 hover:-translate-y-2 cursor-pointer" />
                     </a>
-                    <a href="https://www.linkedin.com/in/manav-rohilla-5834b7247/" target="_blank" rel="noopener noreferrer">
-                         <FaLinkedin className="h-6 lg:h-10 w-8 lg:w-8 duration-200 hover:-translate-y-2 cursor-pointer" />
+                    <a href="https://twitter.com/sh17va" target="_blank" rel="noopener noreferrer">
+                        <TwitterLogoIcon className=" h-6 lg:h-10 w-8 lg:w-8 duration-200 hover:-translate-y-2 cursor-pointer" />
                     </a>
-                    <a href="mailto:manavrohilla3@gmail.com?body=Hello" target="_blank" rel="noopener noreferrer">
+                    <a href="mailto:itsshivahere@outlook.com?body=Hello" target="_blank" rel="noopener noreferrer">
                         <MailPlus className=" h-6 lg:h-10 w-8 lg:w-8 duration-200 hover:-translate-y-2 cursor-pointer" />
                     </a>
-                    <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://instagram.com/shivahereig" target="_blank" rel="noopener noreferrer">
                         <InstagramLogoIcon className=" h-6 lg:h-10 w-8 lg:w-8 duration-200 hover:-translate-y-2 cursor-pointer" />
                     </a>
                 </div>
-                <p className="text-md lg:text-2xl whitespace-break-spaces font-normal">
-                  I&apos;m excited to connect with others via email (
-                  <a href="mailto:manavrohilla3@gmail.com?body=Hello" rel="noopener noreferrer" target="_blank" className="text-blue-500 italic font-bold">
-                        manavrohilla3@gmail.com
-                  </a>
-                  ) and{" "}
-  <a
-    href="https://www.linkedin.com/in/manav-rohilla-5834b7247/"
-    rel="noopener noreferrer"
-    target="_blank"
-    className="font-bold italic text-blue-500"
-  >
-    LinkedIn
-  </a>{" "}
-  to chat about projects and ideas. I&apos;m taking on freelance projects and
-  am open to hearing about potential opportunities, discussing them with you,
-  and then potentially collaborating if it&apos;s a good fit.
-</p>
-
+                <p className=" text-md lg:text-2xl whitespace-break-spaces font-normal">
+                    I&apos;m excited to connect with others via email (
+                    <a href="mailto:itsshivahere@outlook.com?body=Hello" rel="noopener noreferrer" target="_blank" className=" text-blue-500 italic font-bold">
+                        itsshivahere@outlook.com
+                    </a>
+                    ) and{" "}
+                    <a href="https://twitter.com/sh17va" rel="noopener noreferrer" target="_blank" className="font-bold italic text-blue-500">
+                        Twitter
+                    </a>{" "}
+                    to chat about projects and ideas I&apos;m taking on freelance projects and I am open to hearing about potential opportunities, discussing them with you and then potentially collaborating if it&apos;s a good fit.
+                </p>
             </div>
         </section>
     );
